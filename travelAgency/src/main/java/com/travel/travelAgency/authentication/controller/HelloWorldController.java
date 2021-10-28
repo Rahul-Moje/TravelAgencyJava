23c3408fdf6060ac760c15f592d1de21b4802d15
Rahul Moje <rh547954@dal.ca>
2021-10-28 15:56:10 -0300

    Updating controller

--- travelAgency/src/main/java/com/travel/travelAgency/authentication/controller/HelloWorldController.java
+++ travelAgency/src/main/java/com/travel/travelAgency/authentication/controller/HelloWorldController.java
@@ -1,15 +1,13 @@
 package com.travel.travelAgency.authentication.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @RestController
 public class HelloWorldController {
 
-	
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+
+	@GetMapping("/")
 	String home() {
 		return "Hello World!";
 	}
