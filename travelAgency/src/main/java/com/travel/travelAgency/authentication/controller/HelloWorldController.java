3002af144e76a2bc42a7f85c59aabfb972f4fe4a
Rahul Sunil Moje <rh547954@dal.ca>
2021-10-28 15:48:35 -0300

    Update HelloWorldController.java

--- travelAgency/src/main/java/com/travel/travelAgency/authentication/controller/HelloWorldController.java
+++ travelAgency/src/main/java/com/travel/travelAgency/authentication/controller/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.travel.travelAgency.authentication.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @RestController
 public class HelloWorldController {
