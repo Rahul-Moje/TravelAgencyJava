3438946cd0a79088c56545f0cb496f0682d34a57
Sowjanya Mani <sw891064@dal.ca>
2021-11-26 15:54:30 +0530

    Test cases

--- travelAgency/src/test/java/com/travel/travelAgency/grievanceTest/grievanceImplementationTest.java
+++ travelAgency/src/test/java/com/travel/travelAgency/grievanceTest/grievanceImplementationTest.java
@@ -1,5 +1,6 @@
 package com.travel.travelAgency.grievanceTest;
 
+import static org.junit.Assert.assertNotNull;
 import static org.mockito.ArgumentMatchers.anyInt;
 
 import java.sql.Connection;
@@ -13,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.travel.travelAgency.grievance.interfaces.GrievanceInterface;
+import com.travel.travelAgency.grievance.manager.GrievanceManager;
 
 @SpringBootTest
 public class grievanceImplementationTest {
@@ -21,12 +23,20 @@ public class grievanceImplementationTest {
 	GrievanceInterface grievanceinterface;
 	
 	@Test
+	public void grievanceClassExist() {
+		GrievanceManager grievancemanager = new GrievanceManager();
+		assertNotNull(grievancemanager);
+
+
+	}
+	
+	@Test
 	public void RegisterGrievanceComplaintTest() throws Exception {
 		String email = "user1@dal.ca";
 		String complaint = "clean the flight";
 		String SQL = "INSERT INTO grievance (user_email_id,complaint) VALUES ('" + email
 				+ "','" + complaint + "')";
-		Connection conn = Mockito.mock(Connection.class);
+		//Connection conn = Mockito.mock(Connection.class);
 		ResultSet resultSet = Mockito.mock(ResultSet.class);
 		PreparedStatement statement = Mockito.mock(PreparedStatement.class);
 		try {
