5376a760bc0988cb15d0000f63f9b41075825652
Sowjanya Mani <sw891064@dal.ca>
2021-11-26 18:05:29 +0530

    Test

--- travelAgency/src/test/java/com/travel/travelAgency/grievanceTest/grievanceImplementationTest.java
+++ travelAgency/src/test/java/com/travel/travelAgency/grievanceTest/grievanceImplementationTest.java
@@ -1,7 +1,6 @@
 package com.travel.travelAgency.grievanceTest;
 
 import static org.junit.Assert.assertNotNull;
-import static org.mockito.Mockito.doNothing;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -9,57 +8,41 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.travel.travelAgency.grievance.interfaces.GrievanceInterface;
 import com.travel.travelAgency.grievance.manager.GrievanceImplementation;
-import com.travel.travelAgency.grievance.repository.GrievanceRepository;
 
 @SpringBootTest
 public class grievanceImplementationTest {
 
 	@Autowired
 	GrievanceInterface grievanceinterface;
-	
-//	@Mock
-//	GrievanceRepository repo;
-	
+
 	@Test
 	public void grievanceClassExist() {
 		GrievanceImplementation grievancemanager = new GrievanceImplementation();
 		assertNotNull(grievancemanager);
 
-
 	}
-//	@Test
-//	public void RegisterGrievanceComplaintTest() throws Exception {
-//		String email = "user1@dal.ca";
-//		String complaint = "clean the flight";
-//		doNothing().when(repo).registerGrievance(email, complaint);
-//	
-//	}
-	
+
 	@Test
 	public void RegisterGrievanceComplaint1Test() throws Exception {
 		String email = "user1@dal.ca";
 		String complaint = "clean the flight";
-		String SQL = "INSERT INTO grievance (user_email_id,complaint) VALUES ('" + email
-				+ "','" + complaint + "')";
+		String SQL = "INSERT INTO grievance (user_email_id,complaint) VALUES ('" + email + "','" + complaint + "')";
 		Connection conn = Mockito.mock(Connection.class);
 		ResultSet resultSet = Mockito.mock(ResultSet.class);
 		PreparedStatement statement = Mockito.mock(PreparedStatement.class);
 		try {
 			Mockito.when(statement.executeUpdate(SQL)).thenReturn(1);
-			} catch (SQLException e) {
-			
+		} catch (SQLException e) {
+
 			e.printStackTrace();
 		}
 
 	}
-	
-	
-}
 
+}
--- travelAgency/src/test/java/com/travel/travelAgency/promembershipTest/ProMembershipRegistrationImplementationTest.java
+++ travelAgency/src/test/java/com/travel/travelAgency/promembershipTest/ProMembershipRegistrationImplementationTest.java
@@ -1,68 +1,34 @@
 package com.travel.travelAgency.promembershipTest;
 
 import static org.junit.Assert.assertNotNull;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.doNothing;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDateTime;
+
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-
 import com.travel.travelAgency.proMembership.interfaces.ProMembershipRegistrationInterface;
 import com.travel.travelAgency.proMembership.manager.ProMembershipRegistrationImplementation;
-import com.travel.travelAgency.proMembership.models.promembershipregistrationrequest;
-import com.travel.travelAgency.proMembership.repository.ProMembershipRegistrationRepository;
+
 @SpringBootTest
 public class ProMembershipRegistrationImplementationTest {
-	
+
 	@Autowired
 	ProMembershipRegistrationInterface promembershipService;
-	
-//	@Mock
-//	ProMembershipRegistrationRepository repo;
 
 	@Test
 	public void proMembershipClassExistClassExist() {
-	ProMembershipRegistrationImplementation promembershipregistrationimplementation = new ProMembershipRegistrationImplementation();
-	assertNotNull(promembershipregistrationimplementation);
+		ProMembershipRegistrationImplementation promembershipregistrationimplementation = new ProMembershipRegistrationImplementation();
+		assertNotNull(promembershipregistrationimplementation);
 
 	}
-	
-	
-//	@Test
-//	public void verifyEmailandUpdatePlanTest1() throws Exception {
-//		LocalDateTime now = LocalDateTime.now();
-//		String plan_name = "Gold";
-//		String email = "user1@dal.ca";
-//		String date = now.toString();
-//		doNothing().when(repo).updateMembership(email, plan_name, date);
-//	}
 
-	
-//	@Test
-//	public void verifyEmailandUpdatePlanTest2() throws Exception {
-//		LocalDateTime now = LocalDateTime.now();
-//		String plan_name = "Gold";
-//		String email = "user1@dal.ca";
-//		String date = now.toString();
-//		String amount = "1000";
-//		promembershipregistrationrequest request = new promembershipregistrationrequest();
-//		promembershipService.verifyEmailandUpdatePlan(request, repo);
-//		doNothing().when(repo).updateMembership(email, plan_name, date);
-//		
-//	}
-	
-	
-	
-	
 	@Test
 	public void verifyEmailandUpdatePlanTest() throws Exception {
 		LocalDateTime now = LocalDateTime.now();
@@ -72,35 +38,34 @@ public class ProMembershipRegistrationImplementationTest {
 		String SQL1 = "insert into subscriptions_works (user_email_id,plan_name,date_of_purchase) VALUES ('" + email
 				+ "','" + plan_name + "','" + date + ")";
 		String SQL2 = "select plan_cost from pro_membership where plan_name='" + plan_name + "'";
-		 //Connection conn = Mockito.mock(Connection.class);
-		
+		Connection conn = Mockito.mock(Connection.class);
+
 		ResultSet resultSet = Mockito.mock(ResultSet.class);
 		PreparedStatement statement = Mockito.mock(PreparedStatement.class);
 		try {
 			Mockito.when(statement.executeUpdate(SQL1)).thenReturn(1);
 			Mockito.when(statement.executeQuery(SQL2)).thenReturn(resultSet);
-			} catch (SQLException e) {
-			
+		} catch (SQLException e) {
+
 			e.printStackTrace();
 		}
 
 	}
-//	
-//	@Test
-//	public void verifyPlansTest() throws Exception {
-//		String SQL = "select * from pro_membership";
-//		//Connection conn = Mockito.mock(Connection.class);
-//		
-//		ResultSet resultSet = Mockito.mock(ResultSet.class);
-//		PreparedStatement statement = Mockito.mock(PreparedStatement.class);
-//	try {
-//		Mockito.when(statement.executeQuery(SQL)).thenReturn(resultSet);
-//		} catch (SQLException e) {
-//		
-//		e.printStackTrace();
-//	}
-//
-//	}
+
+	@Test
+	public void verifyPlansTest() throws Exception {
+		String SQL = "select * from pro_membership";
+		Connection conn = Mockito.mock(Connection.class);
+
+		ResultSet resultSet = Mockito.mock(ResultSet.class);
+		PreparedStatement statement = Mockito.mock(PreparedStatement.class);
+		try {
+			Mockito.when(statement.executeQuery(SQL)).thenReturn(resultSet);
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+
+	}
 
 }
-
