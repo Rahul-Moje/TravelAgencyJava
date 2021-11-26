9377728b7d6ad44098de56b8d0bd1b98f9364f17
Sowjanya Mani <sw891064@dal.ca>
2021-11-26 16:47:00 +0530

    Test cases

--- travelAgency/src/test/java/com/travel/travelAgency/grievanceTest/grievanceImplementationTest.java
+++ travelAgency/src/test/java/com/travel/travelAgency/grievanceTest/grievanceImplementationTest.java
@@ -3,8 +3,13 @@ package com.travel.travelAgency.grievanceTest;
 import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.doNothing;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -34,26 +39,26 @@ public class grievanceImplementationTest {
 		String complaint = "clean the flight";
 		doNothing().when(repo).registerGrievance(email, complaint);
 	
+	}
 	
-	
-//	@Test
-//	public void RegisterGrievanceComplaintTest() throws Exception {
-//		String email = "user1@dal.ca";
-//		String complaint = "clean the flight";
-//		String SQL = "INSERT INTO grievance (user_email_id,complaint) VALUES ('" + email
-//				+ "','" + complaint + "')";
-//		//Connection conn = Mockito.mock(Connection.class);
-//		ResultSet resultSet = Mockito.mock(ResultSet.class);
-//		PreparedStatement statement = Mockito.mock(PreparedStatement.class);
-//		try {
-//			Mockito.when(statement.executeUpdate(SQL)).thenReturn(Mockito.anyInt());
-//			} catch (SQLException e) {
-//			
-//			e.printStackTrace();
-//		}
-//
-//	}
+	@Test
+	public void RegisterGrievanceComplaint1Test() throws Exception {
+		String email = "user1@dal.ca";
+		String complaint = "clean the flight";
+		String SQL = "INSERT INTO grievance (user_email_id,complaint) VALUES ('" + email
+				+ "','" + complaint + "')";
+		//Connection conn = Mockito.mock(Connection.class);
+		ResultSet resultSet = Mockito.mock(ResultSet.class);
+		PreparedStatement statement = Mockito.mock(PreparedStatement.class);
+		try {
+			Mockito.when(statement.executeUpdate(SQL)).thenReturn(Mockito.anyInt());
+			} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+
+	}
 	
 	
 }
-}
+
