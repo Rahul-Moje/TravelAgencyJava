d1ea9e03260c22ae51ab9906d02d0842eb9d14d9
Sowjanya Mani <sw891064@dal.ca>
2021-11-26 14:42:37 +0530

    Test cases

--- travelAgency/src/test/java/com/travel/travelAgency/grievanceTest/grievanceImplementationTest.java
+++ travelAgency/src/test/java/com/travel/travelAgency/grievanceTest/grievanceImplementationTest.java
@@ -26,12 +26,11 @@ public class grievanceImplementationTest {
 		String complaint = "clean the flight";
 		String SQL = "INSERT INTO grievance (user_email_id,complaint) VALUES ('" + email
 				+ "','" + complaint + "')";
-		try (Connection conn = Mockito.mock(Connection.class)) {
-		}
+		Connection conn = Mockito.mock(Connection.class);
 		ResultSet resultSet = Mockito.mock(ResultSet.class);
 		PreparedStatement statement = Mockito.mock(PreparedStatement.class);
 		try {
-			Mockito.when(statement.executeUpdate(SQL)).thenReturn(anyInt());
+			Mockito.when(statement.executeUpdate(SQL));
 			} catch (SQLException e) {
 			
 			e.printStackTrace();
